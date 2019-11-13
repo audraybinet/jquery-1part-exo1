228790717acc7c0448c9f32f683864f580c98e91
audraybinet <55696156+audraybinet@users.noreply.github.com>
2019-11-13 09:26:07 +0100

    Update script.js
    correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,5 @@
-$("#text").hide();
+$(function(){
+  $('#text').hide();
+  //document.getElementById('text').style.visibility="hidden"
+});
+//on commence par $(function()) retarde l'execution du code avant que la page soit entièrement chargée
